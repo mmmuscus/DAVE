ada4aa28b5d7335ab9255e69627e2211e59b2ec3
mmmuscus <32553610+mmmuscus@users.noreply.github.com>
2019-01-13 20:57:42 +0100

    moar comments...moar commints ;)

--- Dave.cpp
+++ Dave.cpp
@@ -219,7 +219,7 @@ int main()
 					//ha egy sorba van a cuccal akkor top = bottom és right = left, ha alatta vagy felette van akkor pedig teljesen megõrül
 					//balról jobbra nézva ami a player alatt van annak a right és left felcserélõdik
 					//jobbról balra nézve ami a player felett van annak a right és left felcserélõdik
-					//&&&                                                                                  REWRITE azt ami megszerzi a sarkokat!!! think about ur smart algorythm!!! jeeez my man!
+					//&&&                                                              REWRITE azt ami megszerzi a sarkokat!!! think about ur smart algorythm!!! jeeez my man! !!!getLineOfSight this is in actual shadow functions!!!
 					//top one:		left: 62 top: 31 right: 64 bottom: 30
 					//middle one:	left: 60 top: 35 right: 71 bottom: 36
 					//bottom one:	left: 67 top: 39 right: 68 bottom: 40
