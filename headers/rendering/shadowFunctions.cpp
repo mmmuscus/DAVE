91f2085e859044d1de1c0cb0254fce397a9f91f2
mmmuscus <32553610+mmmuscus@users.noreply.github.com>
2019-06-03 19:46:07 +0200

    better?

--- headers/rendering/shadowFunctions.cpp
+++ headers/rendering/shadowFunctions.cpp
@@ -722,10 +722,15 @@ void shadowFunction(map world[WORLDROWS][WORLDCOLS], int cameraCol, int cameraRo
 						{
 							if (isBehindWall(pov, g + cameraRow, h + cameraCol, i + cameraRow, i + cameraRow + 1, j + k + cameraCol, j + cameraCol))
 							{
-								if (/*isBetweenLines(edg.first, edg.second, g + cameraRow, h + cameraCol)/* || */(doesLineIntersectIt(edg.first, g + cameraRow, h + cameraCol) || doesLineIntersectIt(edg.second, g + cameraRow, h + cameraCol)))
+								if (isBetweenLines(edg.first, edg.second, g + cameraRow, h + cameraCol)/* || (doesLineIntersectIt(edg.first, g + cameraRow, h + cameraCol) || doesLineIntersectIt(edg.second, g + cameraRow, h + cameraCol))*/)
 								{
 									world[g + cameraRow][h + cameraCol].mapInView = false;
 								}
+								
+//								if ((doesLineIntersectIt(edg.first, g + cameraRow, h + cameraCol) || doesLineIntersectIt(edg.second, g + cameraRow, h + cameraCol)) && !world[g + cameraRow][h + cameraCol].solid)
+//								{
+//									world[g + cameraRow][h + cameraCol].mapInView = false;
+//								}
 							}
 						}
 					}
@@ -787,10 +792,15 @@ void shadowFunction(map world[WORLDROWS][WORLDCOLS], int cameraCol, int cameraRo
 						{
 							if (isBehindWall(pov, g + cameraRow, h + cameraCol, j + cameraRow, j + k + cameraRow, i + cameraCol + 1, i + cameraCol))
 							{
-								if (/*isBetweenLines(edg.first, edg.second, g + cameraRow, h + cameraCol) || */(doesLineIntersectIt(edg.first, g + cameraRow, h + cameraCol) || doesLineIntersectIt(edg.second, g + cameraRow, h + cameraCol)))
+								if (isBetweenLines(edg.first, edg.second, g + cameraRow, h + cameraCol)/* || (doesLineIntersectIt(edg.first, g + cameraRow, h + cameraCol) || doesLineIntersectIt(edg.second, g + cameraRow, h + cameraCol))*/)
 								{
 									world[g + cameraRow][h + cameraCol].mapInView = false;
 								}
+								
+//								if ((doesLineIntersectIt(edg.first, g + cameraRow, h + cameraCol) || doesLineIntersectIt(edg.second, g + cameraRow, h + cameraCol)) && !world[g + cameraRow][h + cameraCol].solid)
+//								{
+//									world[g + cameraRow][h + cameraCol].mapInView = false;
+//								}
 							}
 						}
 					}
